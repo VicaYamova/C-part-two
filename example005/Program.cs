ad27f0dc443dd2b0529620eb82ee5742be9ef0ae
Vika <vica_che@mail.ru>
2022-08-08 17:28:16 +0300

    hello

---
+++ example005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write ("Введите имя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() ==  "вика")
+{
+ Console.WriteLine("Ура, это же Вика");
+}
+
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
